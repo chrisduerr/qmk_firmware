436e7216a9e2e7d79f33ed6b6601de51e9589304
Christian Duerr <contact@christianduerr.com>
2020-05-17 19:25:40 +0200

    Fix update script

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pacaur -S avr-libc dfu-programmer
+sudo pacman -S avr-libc dfu-programmer
 sudo make xd75:undead:dfu
-pacaur -Rsn avr-libc dfu-programmer
+sudo pacman -Rsn avr-libc dfu-programmer
