c97d72f05ecec56f28b66e36f605841003d694de
Christian Duerr <contact@christianduerr.com>
2022-11-08 07:00:04 +0100

    Add pip to update script

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo pacman -S --noconfirm --needed avr-libc dfu-programmer make qmk
+sudo pacman -S --noconfirm --needed avr-libc dfu-programmer make qmk pip
 sudo pip install pyserial pillow
 
 # sudo make xd75:undead:dfu
@@ -11,4 +11,4 @@ sudo qmk compile -kb undeadsplit -km default
 sudo qmk flash -kb undeadsplit -km default
 
 sudo pip uninstall pyserial pillow
-sudo pacman -Rsn --noconfirm avr-libc dfu-programmer make qmk
+sudo pacman -Rsn --noconfirm avr-libc dfu-programmer make qmk pip
